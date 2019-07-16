4b06adcd04d95e453b47fa9c00815d99f73c8813
marcosdevelo <marcosgomezda@gmail.com>
2019-07-16 01:15:31 +0000

    marcosdevelo

--- script.js
+++ script.js
@@ -6,20 +6,37 @@ let noun = ['jogger','racoon','car','cat','dog'];
 
 
 
+let domain='';
+
+pronoun.forEach((pronoun) => {
+
+    adj.forEach((adj) =>{
+
+        noun.forEach((noun)=>{
+
+            domain += pronoun + adj + noun + '.com' + '<br>';
+
+         });
+
+    });
+
+    });
+//!-----another way of doing it!
+//let arr = []
+
+//for (i=0; i<pronoun.length;i++) {
+   // for (x=0; x<adj.length;x++) {
+     //   for (j=0; j<noun.length;j++) {
+       //     arr.push(pronoun[i]+ adj[x]+ noun[j] + ".com  ")
+      //  }
+    //}
+//}
 
 
-let arr = []
-
-for (i=0; i<pronoun.length;i++) {
-    for (x=0; x<adj.length;x++) {
-        for (j=0; j<noun.length;j++) {
-            arr.push(pronoun[i]+ adj[x]+ noun[j] + ".com  ")
-        }
-    }
-}
 
 
 
 
-document.querySelector('#excuse').innerHTML= arr;
-}
+
+document.querySelector('#excuse').innerHTML= domain;
+};
